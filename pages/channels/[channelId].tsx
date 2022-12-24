4ad364e8360fba34cca53d77c2acd0b66c58d669
tatenodev <gd28.c.tateno@gmail.com>
2022-12-25 00:04:25 +0900

    add style

--- pages/channels/[channelId].tsx
+++ pages/channels/[channelId].tsx
@@ -1,4 +1,5 @@
 import { useRouter } from "next/router";
+import { css } from "@emotion/css";
 
 export default function Channel() {
   const router = useRouter();
@@ -6,15 +7,15 @@ export default function Channel() {
   console.log(`channelId: ${channelId}`);
 
   return (
-    <div>
-      <nav>
+    <div className={Container}>
+      <nav className={Nav}>
         <div>ユーザー名</div>
         <ul>
           <li>todo</li>
           <li>idea</li>
         </ul>
       </nav>
-      <main>
+      <main className={Main}>
         <h1>チャンネルタイトル</h1>
         <div>text log</div>
         <div>text log</div>
@@ -28,3 +29,13 @@ export default function Channel() {
     </div>
   );
 }
+
+const Container = css`
+  display: flex;
+`;
+const Nav = css`
+  flex-shrink: 0;
+`;
+const Main = css`
+  width: 100%;
+`;
